dd0e92266a84cb921530236e6df9aa66176f2507
Abin VK <vettikkadanabin@gmail.com>
2026-03-09 22:07:10 +0530

    frontend

---
+++ Online Book Store/script.js
@@ -0,0 +1,23 @@
+let buttons = document.querySelectorAll(".open-book");
+
+buttons.forEach(function(btn){
+
+btn.onclick = function(){
+
+let title = this.dataset.title;
+let author = this.dataset.author;
+let desc = this.dataset.desc;
+
+document.getElementById("title").innerText = title;
+document.getElementById("author").innerText = author;
+document.getElementById("desc").innerText = desc;
+
+document.getElementById("bookModal").style.display = "block";
+
+}
+
+});
+
+function closeModel(){
+document.getElementById("bookModal").style.display = "none";
+}
